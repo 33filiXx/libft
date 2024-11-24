2e840f64c703ff5eeb24f0d5f729fd6663294744
33filiXx <145553017+33filiXx@users.noreply.github.com>
2024-11-24 14:18:32 +0100

    Update ft_strjoin.c
    handle segfault

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -40,12 +40,12 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	int		ls1;
 	char	*p;
 
+	if (!s1 && !s2)
+		return (NULL);
 	if (!s1)
 		return (ft_strdup(s2));
 	if (!s2)
 		return (ft_strdup(s1));
-	if (!s1 && !s2)
-		return (NULL);
 	ls1 = ft_strlen(s1);
 	ls2 = ft_strlen(s2);
 	p = (char *)malloc((ls1 + ls2 + 1) * sizeof(char));
